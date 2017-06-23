5adde0c4a1b21b5a596cfb75f24deb26fcbcce2e
Maxime Lafarie <maxime.lafarie@biig.fr>
2017-06-23 17:58:42 +0200

    Add build

--- dist/ngx-smart-modal.d.ts
+++
@@ -1,3 +0,0 @@
-export { NgxSmartModalComponent } from './ngx-smart-modal.component';
-export { NgxSmartModalService } from './ngx-smart-modal.service';
-export { NgxSmartModalModule } from './ngx-smart-modal.module';
--- dist/ngx-smart-modal.module.d.ts
+++ dist/ngx-smart-modal.module.d.ts
@@ -1,2 +1,4 @@
+export { NgxSmartModalComponent } from './ngx-smart-modal.component';
+export { NgxSmartModalService } from './ngx-smart-modal.service';
 export declare class NgxSmartModalModule {
 }
