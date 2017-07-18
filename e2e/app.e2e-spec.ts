ae52536e0f4b10c8f02c17b17822d1e38e45fd96
Maxime Lafarie <maxime.lafarie@biig.fr>
2017-07-18 09:59:11 +0200

    Upgrade version and build fix (upgrade rxjs)

--- e2e/app.e2e-spec.ts
+++ e2e/app.e2e-spec.ts
@@ -6,9 +6,4 @@ describe('ngx-smart-modal App', () => {
   beforeEach(() => {
     page = new NgxSmartModalPage();
   });
-
-  it('should display welcome message', () => {
-    page.navigateTo();
-    expect(page.getParagraphText()).toEqual('Welcome to app!!');
-  });
 });
