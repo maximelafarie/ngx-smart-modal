8fd23c1150cabb05312e26107eba8a4af9f0ac2c
thomascsd <thomascsd808@gmail.com>
2017-09-24 22:17:11 +0800

    Fix lazy load feature module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,22 @@
-import {BrowserModule} from '@angular/platform-browser';
-import {NgModule} from '@angular/core';
-import {NgxSmartModalModule, NgxSmartModalService} from '../ngx-smart-modal';
-
-import {AppComponent} from './app.component';
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { RouterModule } from '@angular/router';
+import { AppRoutes } from './app.routing';
+import { NgxSmartModalModule, NgxSmartModalService } from '../ngx-smart-modal';
+import { AppComponent } from './app.component';
+import { MainComponent } from './main/main.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainComponent
   ],
   imports: [
     BrowserModule,
+    BrowserAnimationsModule,
+    RouterModule,
+    AppRoutes,
     NgxSmartModalModule.forRoot()
   ],
   providers: [NgxSmartModalService],
