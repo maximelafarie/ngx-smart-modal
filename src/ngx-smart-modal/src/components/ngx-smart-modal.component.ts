2c325afef266a27660ecd20e4619d5310f79755b
Darko <darkomtc11@gmail.com>
2019-10-03 16:11:59 +0200

    Dragging methods are now public

--- src/ngx-smart-modal/src/components/ngx-smart-modal.component.ts
+++ src/ngx-smart-modal/src/components/ngx-smart-modal.component.ts
@@ -165,7 +165,7 @@ export class NgxSmartModalComponent implements OnInit, OnDestroy, AfterViewInit 
   * Listens for mouse down event to initiate dragging of the modal
   */
   @HostListener('document:mousedown', ['$event'])
-  startDrag(e: MouseEvent) {
+  public startDrag(e: MouseEvent) {
     if (!this.nsmContent.length || !this.draggable) {
       return false;
     }
@@ -189,7 +189,7 @@ export class NgxSmartModalComponent implements OnInit, OnDestroy, AfterViewInit 
   * Listens for mouse move event and reflects the movement of the mouse to modal position
   */
   @HostListener('document:mousemove', ['$event'])
-  elementDrag(e: MouseEvent) {
+  public elementDrag(e: MouseEvent) {
     if (!this.dragging || !this.nsmDialog.length) {
       return false;
     }
@@ -210,7 +210,7 @@ export class NgxSmartModalComponent implements OnInit, OnDestroy, AfterViewInit 
   * Listens for mouse up event to stop moving dragged modal
   */
   @HostListener('document:mouseup', ['$event'])
-  stopDrag() {
+  public stopDrag() {
     this.dragging = false;
   }
 
